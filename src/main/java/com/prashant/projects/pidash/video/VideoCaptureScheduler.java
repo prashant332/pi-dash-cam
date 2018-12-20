e4c12256cb9256fa2e6e19a997fe5d31ee0f379b
Prashant Hegde <prashant.hegde@sabre.com>
2018-12-20 21:54:42 +0530

    Initial commit

--- src/main/java/com/prashant/projects/pidash/video/VideoCaptureScheduler.java
+++ src/main/java/com/prashant/projects/pidash/video/VideoCaptureScheduler.java
@@ -35,6 +35,7 @@ public class VideoCaptureScheduler {
 
     private void startCapture(String fileName) {
         String captureCommand = "raspivid "+ videoAttributesProvider.getVideoAttributes()+" -o /home/pi/capture/"+fileName;
+        System.out.println(">>>>>>>"+captureCommand);
         executeCommand(captureCommand);
     }
 
