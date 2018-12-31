7a5c83fde82f8a7028553fd6940e2ae6e1a4818c
Prashant Hegde <prashant.hegde@sabre.com>
2018-12-31 21:49:31 +0530

    removed unused codes

--- src/main/java/com/prashant/projects/pidash/repo/SettingsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.prashant.projects.pidash.repo;
-
-import com.prashant.projects.pidash.model.Settings;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SettingsRepo extends JpaRepository<Settings, Long> {
-}
